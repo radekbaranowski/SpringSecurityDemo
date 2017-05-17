e37b99dbea58a8e5d52d75797bb01de3c13245da
Radoslaw Baranowski <radoslaw.baranowski@cgius.com>
2017-05-17 14:40:23 +0200

    custom login

--- src/main/java/demo/springsecurity/HomeController.java
+++ src/main/java/demo/springsecurity/HomeController.java
@@ -3,6 +3,7 @@ package demo.springsecurity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -25,10 +26,28 @@ public class HomeController {
 		System.out.println("in controller");
 		ModelAndView model = new ModelAndView();
 		model.addObject("title", "Spring security demo");
-		model.addObject("message", "this is ADMIN page");
+		model.addObject("message", "this is proteted ADMIN page");
 		model.setViewName("admin");
 		return model;
 		
 	}
 
+	
+	@RequestMapping(value = {"/login"}, method = RequestMethod.GET)
+	public ModelAndView login(
+			@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout){	
+		System.out.println("in login controller");
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", "Invalid username and password!");
+		}
+
+		if (logout != null) {
+			model.addObject("msg", "You've been logged out successfully.");
+		}
+		model.setViewName("login");
+		return model;
+		
+	}
 }
--- src/main/webapp/WEB-INF/pages/hello.jsp
+++ src/main/webapp/WEB-INF/pages/hello.jsp
@@ -4,5 +4,7 @@
 <body>
 	<h1>Title : ${title}</h1>
 	<h1>Message : ${message}</h1>
+	
+	<p>Goto <a href ="admin">Admin </a></p>
 </body>
 </html>
